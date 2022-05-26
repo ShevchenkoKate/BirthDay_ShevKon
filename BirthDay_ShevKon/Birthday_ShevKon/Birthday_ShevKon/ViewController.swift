1b3f5a0fa19fa561ff1e2a7c09e4f6bac82afea3
Ekaterina Shevchenko <75583677+ShevchenkoKate@users.noreply.github.com>
2022-05-26 14:35:29 +0300

    Add files via upload

---
+++ BirthDay_ShevKon/Birthday_ShevKon/Birthday_ShevKon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Birthday_ShevKon
+//
+//  Created by Student on 26.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
